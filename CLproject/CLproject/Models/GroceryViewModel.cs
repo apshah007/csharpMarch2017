f1ce09b01d40aeff792a77d1051954669d6a8161
apshah007 <apshah007@yahoo.com>
2017-03-23 19:41:38 -0400

    changes the min length of things added to database

--- CLproject/CLproject/Models/GroceryViewModel.cs
+++ CLproject/CLproject/Models/GroceryViewModel.cs
@@ -28,6 +28,7 @@ namespace CLproject.Models
         public int ItemId { get; set; }
         [DisplayName("Product Name")]
         [MaxLength(50, ErrorMessage = "Max length is 50 characters")]
+        [MinLength(3, ErrorMessage= "Min length is 3 characters")]
         public string ProductName { get; set; }
 
         public bool InCart { get; set; }
